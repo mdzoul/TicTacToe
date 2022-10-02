0e09efcf281bfecee3ed834b4ab72124e840f197
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-02 19:58:53 +0000

    Added player2 function and fixed draws

--- main.py
+++ main.py
@@ -22,10 +22,16 @@ def replace_spot(pla_com):
         for value in list:
             if value == pla_com:
                 value_idx = list.index(value)
-                if pla_com == p1:
-                    board[list_idx][value_idx] = '\33[1;31mX\33[0m'
-                elif pla_com == com:
-                    board[list_idx][value_idx] = '\33[1;34mO\33[0m'
+                if p1_vs == '1':
+                    if pla_com == p1:
+                        board[list_idx][value_idx] = '\33[1;31mX\33[0m'
+                    elif pla_com == com:
+                        board[list_idx][value_idx] = '\33[1;34mO\33[0m'
+                elif p1_vs == '2':
+                    if pla_com == p1:
+                        board[list_idx][value_idx] = '\33[1;31mX\33[0m'
+                    elif pla_com == p2:
+                        board[list_idx][value_idx] = '\33[1;34mO\33[0m'
 
 def check_board():
     # Horizontal
@@ -55,6 +61,9 @@ def check_board():
         return True
     if board[0][2] == '\33[1;34mO\33[0m' and board[1][2] == '\33[1;34mO\33[0m' and board[2][2] == '\33[1;34mO\33[0m':
         return True
+    #Draw
+    if list_num == []:
+        return True
 
 def refresh():
     clear()
@@ -65,44 +74,97 @@ board = [
     ['4','5','6'],
     ['7','8','9'],
 ]    
-
 list_num = ['1','2','3','4','5','6','7','8','9']
 
 logo()
-display_board()
 
-end_game = False
-while not end_game:
-    if check_board() == True:
-        end_game = True
-        print("\33[31mGame Over\33[0m")
-        break
+p1_vs = input("How many players?: ")
 
-    while check_board() != True:
-        p1 = input("Your turn: ")
-        list_num.remove(p1)
-        replace_spot(p1)
-        
-        refresh()
-        
-        display_board()
-        check_board()
-
-        time.sleep(1.5)
-
-        if check_board() != True:
-            com = str(random.choice(list_num))
-            list_num.remove(com)
-            replace_spot(com)
-            
-            refresh()
-            print("Computer's turn:")
-            time.sleep(1)
-            
-            display_board()
-            check_board()
-
-            time.sleep(1.5)
-        else:
+clear()
+if p1_vs == '1':
+    logo()
+    display_board()
+    
+    end_game = False
+    while not end_game:
+        if check_board() == True:
+            end_game = True
+            print("\33[31mGame Over\33[0m")
             break
+    
+        while check_board() != True:
+            if list_num != []:
+                p1 = input("Your turn: ")
+                list_num.remove(p1)
+                replace_spot(p1)
+                
+                refresh()
+                
+                display_board()
+                check_board()
         
+                time.sleep(1)
+        
+                if check_board() != True:
+                    if list_num != []:
+                        com = str(random.choice(list_num))
+                        list_num.remove(com)
+                        replace_spot(com)
+                        
+                        refresh()
+                        print("Computer's turn:")
+                        time.sleep(1)
+                        
+                        display_board()
+                        check_board()
+            
+                        time.sleep(1)
+                    else:
+                        break
+                else:
+                    break
+            else:
+                break
+        
+elif p1_vs == '2':
+    logo()
+    display_board()
+    
+    end_game = False
+    while not end_game:
+        if check_board() == True:
+            end_game = True
+            print("\33[31mGame Over\33[0m")
+            break
+    
+        while check_board() != True:
+            if list_num != []:
+                p1 = input("Player 1's turn: ")
+                list_num.remove(p1)
+                replace_spot(p1)
+                
+                refresh()
+                
+                display_board()
+                check_board()
+        
+                time.sleep(1)
+        
+                if check_board() != True:
+                    if list_num != []:
+                        p2 = input("Player 2's turn: ")
+                        list_num.remove(p2)
+                        replace_spot(p2)
+                        
+                        refresh()
+                        
+                        display_board()
+                        check_board()
+                
+                        time.sleep(1)
+                    else:
+                        break
+                else:
+                    break
+            else:
+                break
